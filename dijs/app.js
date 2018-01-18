7c034c2a40847ef05721ce513b6377903c2017d0
Vittorio Iacovella <vittorio.iacovella@gmail.com>
2018-01-18 17:03:47 +0100

    Injector lesson

---
+++ dijs/app.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+angular.module('DIApp', [])
+.controller('DIController', DIController);
+
+function DIController ($scope, $filter, $injector) {
+  $scope.name = "Vittorio";
+
+  $scope.upper = function() {
+
+    var upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name)
+
+  };
+
+  console.log($injector.annotate(DIController));
+
+}
+
+function AnnotateMe(name, job, blah){
+
+return "Blah!"
+
+}
+
+console.log(DIController.toString());
+
+})();
